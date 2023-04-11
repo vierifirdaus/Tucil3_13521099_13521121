5cff2bc28b41f66c437e23c40cef7a8167e76fb5
SaddamAnnais <saddamannaiss@gmail.com>
2023-04-11 15:02:24 +0700

    fix: py converter file

--- test1.py
+++ test1.py
@@ -5,6 +5,6 @@ place_name = "Dago, Bandung, Indonesia"
 
 # download the road network
 G = ox.graph_from_place(place_name, network_type='drive')
-
+G_undirected = ox.utils_graph.get_undirected(G)
 # save the graph in GraphML format
-ox.save_graphml(G, "san_francisco_drive.graphml")
+ox.save_graphml(G_undirected, "dago.graphml", gephi=True)
